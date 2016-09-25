29b8ac425aba8f953f362027b4c77f8a49377068
Joshua Lee <josh.koo.lee@gmail.com>
2016-09-24 21:16:58 -0400

    aj

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // add a comment 
         // this comment from Peter
-        // i liek pie
+        // i liek pie!
     }
 
     override func didReceiveMemoryWarning() {
