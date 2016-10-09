386a1bc92123013d13c4b7229ae965e7a7577b3a
Joshua Lee <josh.koo.lee@gmail.com>
2016-10-08 21:08:43 -0400

    Added more things like the AI and Reset Button

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var ticTacImg1: UIImageView!
     @IBOutlet var ticTacImg2: UIImageView!
+    @IBOutlet var thisIsATest: UIImageView!
     @IBOutlet var ticTacImg3: UIImageView!
     @IBOutlet var ticTacImg4: UIImageView!
     @IBOutlet var ticTacImg5: UIImageView!
@@ -40,7 +41,7 @@ class ViewController: UIViewController {
     var done = false
     var aiDeciding = false
     
-    @IBAction func UIButtonClicked(sender: UIButton){
+      @IBAction func UIButtonClicked(sender: UIButton){
         userMessage.isHidden = true
         if !(plays [sender.tag] != nil) && !aiDeciding && !done {
             setImageForSpot(spot: sender.tag, player:1)
@@ -51,7 +52,7 @@ class ViewController: UIViewController {
     }
     
     func setImageForSpot(spot:Int, player:Int){
-        var playerMark = player == 1 ? "x" : "o"
+        let playerMark = player == 1 ? "x" : "o"
         plays[spot] = player
         switch spot{
         case 1:
@@ -76,16 +77,71 @@ class ViewController: UIViewController {
             ticTacImg5.image = UIImage(named: playerMark)
             
         }
-        
+   
     }
-    
     func checkForWin(){
+        let whoWon = ["I":0, "you":1]
+        for(key,value) in whoWon {
+            if ((plays[7] == value && plays[8] == value && plays[9] == value) || //across the bottom)
+            (plays[4] == value && plays[5] == value && plays[6] == value) || //across the middle)
+            (plays[1] == value && plays[2] == value && plays[3] == value) || //accross the top)
+            (plays[1] == value && plays[4] == value && plays[7] == value) || //down the left)
+            (plays[3] == value && plays[6] == value && plays[9] == value) || //down the right)
+            (plays[2] == value && plays[5] == value && plays[8] == value) || //down the middle)
+            (plays[1] == value && plays[5] == value && plays[9] == value) || //diag left right)
+                (plays[3] == value && plays[5] == value && plays[7] == value)){ //diag right left
+                    userMessage.isHidden = false
+                    userMessage.text = "Looks like \(key) won!"
+                    resetBtn.isHidden = false
+                    done = true
+            }
+        }
         
     }
     
+    @IBAction func resetBtnClicked(sender:UIButton){
+        done = false
+        resetBtn.isHidden = true
+        userMessage.isHidden = true
+        reset()
+    }
+    func reset() {
+        plays = [:]
+        ticTacImg1.image = nil
+        ticTacImg2.image = nil
+        ticTacImg3.image = nil
+        ticTacImg4.image = nil
+        ticTacImg5.image = nil
+        ticTacImg6.image = nil
+        ticTacImg7.image = nil
+        ticTacImg8.image = nil
+        ticTacImg9.image = nil
+        
+    }
+    func rowCheck(#value:Int) -> (location:String, pattern:String)? {
+        var acceptableFinds = ["011", "110", "101"]
+        var findFuncs = [checkTop, checkBottom, checkLeft, checkRight, checkMiddleAcross, checkMiddleDown, checkDiagLeftRight, checkDiagRightLeft]
+        for algorithm in findFuncs{
+            var algorithmResults = algorithm(value:value)
+            if find(acceptableFinds, algorithmResults.pattern)
+                return algorithmResults
+        }
+    }
     
     func aiTurn(){
+        if done {
+            return
+        }
         
+        aiDeciding = true
+        
+      
+        //We (the computer) have two in a row
+        if let result = rowCheck(value:0) {
+            
+        }
+        
+        aiDeciding = false
     }
     
     override func viewDidLoad() {
@@ -99,6 +155,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
